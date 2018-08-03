28a6ebeb3fa80e6752ee705997573f7df084541c
cindyaddoil <cindyaddoil@gmail.com>
2018-08-03 00:56:30 -0700

    Update Binary Files.cpp

--- StructPadding/src/Binary Files.cpp
+++ StructPadding/src/Binary Files.cpp
@@ -9,23 +9,37 @@
 #include <iostream>
 #include <fstream>
 using namespace std;
-
 #pragma pack(push, 1)
-
 struct Person {
 	char name[50];
 	int age;
-	double weight;
 };
-
 #pragma pack(pop)
 
 int main() {
+	Person baobao = {"baobao" , 5};
+	fstream outFile;
+	outFile.open("text.bin", ios::binary | ios::out);
+	if (outFile.is_open()) {
+		outFile.write((char*)&baobao, sizeof(Person));
+		outFile.close();
+	} else {
+		cout << "can't open the file to write" << endl;
+	}
 
-	cout << sizeof(Person) << endl;
+	Person other = {};
+	fstream inFile;
+	inFile.open("text.bin", ios::binary | ios::in);
 
-
-
+	if (inFile.is_open()) {
+		string line;
+		inFile.read((char*)&other, sizeof(Person));
+		inFile.close();
+	} else {
+		cout << "can't open the file to read" << endl;
+	}
+	cout << other.name;
+	cout << " " << other.age << endl;
 
 	return 0;
 }
